dffab70dc2d102973f7237f0186068c357c7f155
DidO <77dido@github.com>
2025-11-24 15:04:06 +0100

    chore: ignore openwebui data artifacts

--- openwebui_data/functions/reg_toggle_action.py
+++
@@ -1,9 +0,0 @@
-from openwebui.functions import action
-
-@action(label="Toggle RAG", icon="search")
-def toggle_rag(message, context):
-    # Alterne le champ use_rag dans metadata
-    metadata = message.get("metadata", {})
-    metadata["use_rag"] = not metadata.get("use_rag", True)
-    message["metadata"] = metadata
-    return message
